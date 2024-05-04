4fb1c8e49f831c9b30ef044d866b6a5d73265b9b
Adriansyah Ramadhan <89848584+AdrianStudent@users.noreply.github.com>
2024-05-04 13:31:57 +0700

    perubahan stegano

--- dsa.py
+++ dsa.py
@@ -1,8 +1,9 @@
-# Import necessary libraries
 from Crypto.PublicKey import DSA
 from Crypto.Signature import DSS
 from Crypto.Hash import SHA256
 from stegano import lsb
+from PIL import Image
+import numpy as np
 
 # Set constants
 IMAGE_FILE = 'helicopter.jpg'  # Input image file
@@ -54,8 +55,74 @@ def verify_signature():
 
 # Perform BPCS steganography
 def steganograph_image():
+    # Fungsi BPCS steganografi warna
+    def hide_data(image, data):
+        # Pisahkan gambar ke dalam komponen warna RGB
+        red, green, blue = image.split()
+        
+        # Konversi komponen warna ke dalam array numpy
+        red_np = np.array(red)
+        green_np = np.array(green)
+        blue_np = np.array(blue)
+        
+        # Pisahkan komponen warna ke dalam bit-plane
+        red_bit_planes = [(red_np & (1 << i)) >> i for i in range(8)]
+        green_bit_planes = [(green_np & (1 << i)) >> i for i in range(8)]
+        blue_bit_planes = [(blue_np & (1 << i)) >> i for i in range(8)]
+        
+        # Ubah data ke dalam format biner
+        data_bits = [int(bit) for bit in ''.join(format(byte, '08b') for byte in data)]
+        
+        # Cari bit-plane dengan kompleksitas tinggi untuk masing-masing komponen warna
+        def calculate_complexity(bit_plane):
+            height, width = bit_plane.shape
+            complexity = 0
+            # Hitung perubahan bit (dari 0 ke 1 atau 1 ke 0) dalam bit-plane
+            for i in range(height):
+                for j in range(width - 1):
+                    complexity += abs(bit_plane[i, j] - bit_plane[i, j + 1])
+            for j in range(width):
+                for i in range(height - 1):
+                    complexity += abs(bit_plane[i, j] - bit_plane[i + 1, j])
+            return complexity / (height * width)
+        
+        def embed_data_in_bit_plane(bit_planes, data_bits):
+            for i in range(7, -1, -1):
+                complexity = calculate_complexity(bit_planes[i])
+                if complexity > 0.3:  # Tetapkan batas kompleksitas untuk embed
+                    index = 0
+                    for r in range(bit_planes[i].shape[0]):
+                        for c in range(bit_planes[i].shape[1]):
+                            if index < len(data_bits):
+                                bit_planes[i][r, c] = data_bits[index]
+                                index += 1
+                    return bit_planes
+        
+        # Embed data ke dalam bit-plane dengan kompleksitas tinggi
+        red_bit_planes = embed_data_in_bit_plane(red_bit_planes, data_bits)
+        green_bit_planes = embed_data_in_bit_plane(green_bit_planes, data_bits)
+        blue_bit_planes = embed_data_in_bit_plane(blue_bit_planes, data_bits)
+        
+        # Rekonstruksi gambar dengan bit-plane yang dimodifikasi
+        red = sum((plane << i) for i, plane in enumerate(red_bit_planes))
+        green = sum((plane << i) for i, plane in enumerate(green_bit_planes))
+        blue = sum((plane << i) for i, plane in enumerate(blue_bit_planes))
+        
+        # Gabungkan kembali gambar yang telah dimodifikasi
+        new_image = Image.merge('RGB', (Image.fromarray(red), Image.fromarray(green), Image.fromarray(blue)))
+        return new_image
+    
+    # Baca gambar
+    image = Image.open(IMAGE_FILE)
+    
+    # Pesan rahasia yang akan disisipkan
     secret_message = "This is a secret message"
-    steg_image = lsb.hide(IMAGE_FILE, secret_message)
+    secret_data = secret_message.encode()  # Ubah pesan menjadi biner
+    
+    # Sembunyikan data menggunakan BPCS steganografi warna
+    steg_image = hide_data(image, secret_data)
+    
+    # Simpan gambar yang telah dimodifikasi
     steg_image.save(STEALTH_IMAGE_FILE)
 
 # Main function
